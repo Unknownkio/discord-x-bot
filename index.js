4147f5fcd6222cc4b684970ad56ebb52ce00cfa2
Unknownkio <christianwojta22@icloud.com>
2026-02-11 22:09:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+const TWITTER_USER_ID = process.env.TWITTER_USER_ID;
+ import { Client, GatewayIntentBits } from "discord.js";
 import axios from "axios";
 import dotenv from "dotenv";
  const response = await axios.get(
